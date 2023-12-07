e331a1fea6529b0b5cf44747ef41a7184fa9fa28
houyem ben arbia <benarbia.houyem62@gmail.com>
2023-12-07 20:12:32 +0100

    supp plt

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pickle
 import requests
 import streamlit.components.v1 as components
-import matplotlib.pyplot as plt
 import seaborn as sns
 from main import data 
 from dashboard import dashboard_page
